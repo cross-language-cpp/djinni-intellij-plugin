341c1749df8f576a9422e7d51c40be1e21c6dc71
GitHub Action <action@github.com>
2020-10-13 19:14:04 +0000

    Template cleanup

--- src/main/kotlin/com/github/jothepro/djinniintellijplugin/MyBundle.kt
+++ src/main/kotlin/com/github/jothepro/djinniintellijplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.jothepro.djinniintellijplugin
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/jothepro/djinniintellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.jothepro.djinniintellijplugin.services
+
+import com.github.jothepro.djinniintellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
